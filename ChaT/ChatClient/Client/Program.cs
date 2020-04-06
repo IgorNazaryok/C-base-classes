4528f4a23c6f812c8ff45659e77f456f0f7ce580
IgorNazaryok <igor.nazarek@gmail.com>
2020-04-06 09:56:22 +0300

    1

---
+++ ChaT/ChatClient/Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tcp client staeted");
+            string massage = "massage";
+            try
+            {
+                var port = 19539;
+                var serverAddress = "127.0.0.1";
+                TcpClient client = new TcpClient(serverAddress, port);
+                NetworkStream stream = client.GetStream(); // создание потока для передачи данных
+                byte[] responseData = new byte[1024];
+                int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                string responseMassage = System.Text.Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                Console.WriteLine(responseMassage);
+                while (true)
+                {
+                    massage = Console.ReadLine();
+                    var data = System.Text.Encoding.ASCII.GetBytes(massage);
+                    stream.Write(data, 0, data.Length); //передача в поток
+                    //Console.WriteLine("Sent {0}", massage);
+
+                    bytesRead = stream.Read(responseData, 0, responseData.Length);
+                    responseMassage = System.Text.Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                    Console.WriteLine("Sent server {0}", responseMassage);
+                    
+                }
+                    stream.Close();
+                    client.Close();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception {0}", e);
+            }
+            
+        }
+    } 
+}
---
+++ ChaT/ChatServer/Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tcp server staeted");
+            try
+            {
+                int port = 19539;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1"); //создания объекта слушателя 
+                TcpListener server = new TcpListener(localAddr, port);
+                server.Start();
+
+                var bytes = new byte[256];
+                string data = "";
+
+                TcpClient client = null;
+
+                NetworkStream stream;
+
+
+                byte[] response;
+                int bytesReadCount;
+                client = server.AcceptTcpClient();
+                Console.WriteLine("Accepted conection from {0}", client.Client.RemoteEndPoint);
+                stream = client.GetStream();  //создаем поток 
+                data = "Hello " + client.Client.RemoteEndPoint;
+                response = System.Text.Encoding.ASCII.GetBytes(data);
+                stream.Write(response, 0, response.Length);
+                while (true)
+                {
+                    //if (server.AcceptTcpClient())
+                    //{
+                    //    Console.WriteLine("Accepted conection from {0}", client.Client.RemoteEndPoint);
+                    //    stream = client.GetStream();  //создаем поток 
+                    //    data = "Hello " + client.Client.RemoteEndPoint;
+                    //    response = System.Text.Encoding.ASCII.GetBytes(data);
+                    //    stream.Write(response, 0, response.Length);
+
+                    //}
+                    //Console.WriteLine(stream.Read(bytes, 0, bytes.Length));
+
+                    //if (stream.Read(bytes, 0, bytes.Length) != 0)
+                    //{
+                    bytesReadCount = stream.Read(bytes, 0, bytes.Length);
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, bytesReadCount);
+                    Console.WriteLine("Sent client {0}", data);
+                    data = data.ToUpper();
+                    response = System.Text.Encoding.ASCII.GetBytes(data);
+                    stream.Write(response, 0, response.Length);
+                    Console.WriteLine("Sent server {0}", data);
+                    //}
+
+
+
+
+                    //client.Close();
+                    // client = null;//создание клиента
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception {0}", e);
+            }
+
+        }
+    }
+}
