35a29807b340a747a80b3ceccd4c11e35e40636b
IgorNazaryok <igor.nazarek@gmail.com>
2020-04-10 09:42:52 +0300

    1

--- async-await/AsyncTest/AsyncTest/Program.cs
+++ async-await/AsyncTest/AsyncTest/Program.cs
@@ -6,20 +6,20 @@ namespace HelloApp
 {
     class Program
     {
-        static void Factorial(int n)
+        static void FileUpload(int n)
         {
             Console.WriteLine("Начата загрузка файла {0} файла",n);         
             Thread.Sleep(3000);            
             Console.WriteLine("Загрузка файла {0} завершена", n);
         }
        
-        static async void FactorialAsync(int n)
+        static async void FileUploadAsync(int n)
         {
             DateTime start = DateTime.Now;
-            Task t1 = Task.Run(() => Factorial(1));
-            Task t2 = Task.Run(() => Factorial(2));
-            Task t3 = Task.Run(() => Factorial(3));
-            Task t4 = Task.Run(() => Factorial(4));
+            Task t1 = Task.Run(() => FileUpload(1));
+            Task t2 = Task.Run(() => FileUpload(2));
+            Task t3 = Task.Run(() => FileUpload(3));
+            Task t4 = Task.Run(() => FileUpload(4));
             await Task.WhenAll(new[] { t1, t2, t3, t4 });
             TimeSpan time = DateTime.Now- start;
             Console.WriteLine("Время загрузки {0} файлов {1} минут, {2} секунд, {3} милисекунд", 4, time.Minutes, time.Seconds, time.Milliseconds);
@@ -29,7 +29,7 @@ namespace HelloApp
             //Task[] task = new Task[n];
             //for (int i = 0; i < n; i++)
             //{
-            //    task[i] = new Task(() => Factorial(i));
+            //    task[i] = new Task(() => FileUpload(i));
             //    task[i].Start();
             //}            
             //await Task.WhenAll(task);
@@ -37,14 +37,14 @@ namespace HelloApp
         }
         static void Main(string[] args)
         {
-            FactorialAsync(4);
+            FileUploadAsync(4);
             for (int i = 0; i < 5; i++)
             {
 
                 Console.Write("*");
                 Thread.Sleep(1000);
             }
-            
+            Console.Read();
         }
     }
 }
--- async-await/SyncTest/SyncTest/Program.cs
+++ async-await/SyncTest/SyncTest/Program.cs
@@ -6,7 +6,7 @@ namespace HelloApp
 {
     class Program
     {
-        static void Factorial(int n)
+        static void FileUpload(int n)
         {
             Console.WriteLine("Начата загрузка файла {0} файла", n);          
             Thread.Sleep(3000);
@@ -20,12 +20,13 @@ namespace HelloApp
             DateTime start = DateTime.Now;
             for (int i = 1; i <= 4; i++)
             {
-                Factorial(i);
+                FileUpload(i);
             }
             TimeSpan time = DateTime.Now - start;
             Console.WriteLine("Время загрузки {0} файлов {1} минут, {2} секунд, {3} милисекунд", 4, time.Minutes, time.Seconds, time.Milliseconds);
 
-
-        }    
+            Console.Read();
+        }
+       
     }
 }
