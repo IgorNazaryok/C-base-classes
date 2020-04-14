757795ef45cae7532d2d7c56c2851c8f161255a8
IgorNazaryok <igor.nazarek@gmail.com>
2020-04-14 21:50:57 +0300

    1

---
+++ HelloMVCapllication/HelloMVCapllication/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMVCapllication.Models
+{
+    public class Answers
+    {
+        public int Id { get; set; }
+        public string Аnswer { get; set; }
+        public string GuestionId { get; set; }
+    }
+}
---
+++ HelloMVCapllication/HelloMVCapllication/Models/Context.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace HelloMVCapllication.Models
+{
+    public interface IUserRepository
+    {
+        void Create(User user);
+        void Delete(int id);
+        User Get(int id);
+        List<User> GetUsers();
+        void Update(User user);
+    }
+    public class UserRepository : IUserRepository
+    {
+        string connectionString = null;
+        public UserRepository(string conn)
+        {
+            connectionString = conn;
+        }
+        public List<User> GetUsers()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users").ToList();
+            }
+        }
+
+        public User Get(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users WHERE Id = @id", new { id }).FirstOrDefault();
+            }
+        }
+
+        public void Create(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "INSERT INTO Users (UserName, Password) VALUES(@UserName, @Password)";
+                db.Execute(sqlQuery, user);
+
+                // если мы хотим получить id добавленного пользователя
+                //var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
+                //int? userId = db.Query<int>(sqlQuery, user).FirstOrDefault();
+                //user.Id = userId.Value;
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "UPDATE Users SET UserName = @UserName, Password = @Password WHERE Id = @Id";
+                db.Execute(sqlQuery, user);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "DELETE FROM Users WHERE Id = @id";
+                db.Execute(sqlQuery, new { id });
+            }
+        }
+    }
+}
